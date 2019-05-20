78b1eed276ea99b48756c98fddd3d70d8701f603
yelmo69 <47380114+yelmo69@users.noreply.github.com>
2019-05-20 11:01:35 +0200

    Update inschrijvingen.sql

--- inschrijvingen.sql
+++ inschrijvingen.sql
@@ -8,3 +8,4 @@ tussenvoegsel, voorletters, voornaam, geboortedatum ,postcode ,plaats
 (99986, 'marmsen', 'h', 'E', 'Janiffer','2003-01-26 00:00:00', '2632 EP','Nootdorp', '65', 170, 1, '2019-01-01');
 
 UPDATE studenten SET geboortedatum = '2003-02-26 00:00:00' WHERE ov_nummer = 99986;
+UPDATE studenten SET actief = FALSE WHERE inschrijvings_datum <= '2017-01-01';
