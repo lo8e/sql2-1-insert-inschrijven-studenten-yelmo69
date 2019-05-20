e53cb85c157356948931a720f55ab4dc5759ff2e
yelmo69 <47380114+yelmo69@users.noreply.github.com>
2019-05-20 09:59:57 +0200

    opdra 4

--- inschrijvingen.sql
+++ inschrijvingen.sql
@@ -6,3 +6,5 @@ tussenvoegsel, voorletters, voornaam, geboortedatum ,postcode ,plaats
 (99988, 'aarmsen', 'h', 'H', 'Janus','2003-03-16 00:00:00', '2632 EP', 'Nootdorp', '65', 170, 1, '2019-01-01'),
 (99987, 'darmsen', 'h', 'D', 'Jannus','2003-02-28 00:00:00', '2632 EP', 'Nootdorp','65', 170, 1, '2019-01-01'),
 (99986, 'marmsen', 'h', 'E', 'Janiffer','2003-01-26 00:00:00', '2632 EP','Nootdorp', '65', 170, 1, '2019-01-01');
+
+UPDATE studenten SET geboortedatum = '2003-02-26 00:00:00' WHERE ov_nummer = 99986;
