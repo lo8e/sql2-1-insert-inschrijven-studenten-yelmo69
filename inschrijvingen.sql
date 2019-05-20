a37dfd34c9484d1cc080c8968defa692a9747cb2
yelmo69 <47380114+yelmo69@users.noreply.github.com>
2019-05-20 09:28:49 +0200

    Opdracht 1

---
+++ inschrijvingen.sql
@@ -0,0 +1,5 @@
+USE lo8e_sql2;
+INSERT INTO klassen (klas_code, slb_code, cohort, opleiding_code) VALUES
+ ('lo9e-amo3', 'rs001', 2019, 'amo'),
+ ('lo9e-amo4', 'vlt03', 2019, 'amo'),
+  ('lo9e-amo5', 'akr03', 2019, 'amo');
